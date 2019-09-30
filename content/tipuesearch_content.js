63f740513a1a627d3d40695481426fba5f525538
40823221 <40823221@mde.tw>
2019-10-01 00:38:31 +0800

    add embedding dartpad

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '', 'tags': '', 'url': 'About.html'}, {'title': 'USB', 'text': '', 'tags': '', 'url': 'USB.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'http://mde.tw/dartpad/?id=32e6b86440c6ba2b3c82411d8b3babd2 \n \n', 'tags': '', 'url': 'About.html'}]};
