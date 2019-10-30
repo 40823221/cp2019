61b1e554905a0b39a0f02343e3453fa88f7a3a9b
40823221 <40823221@mde.tw>
2019-10-31 00:19:23 +0800

    01

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '\n 程式碼: \n void main() {  print("Hello, World!"); } \n \n 程式碼: \n int i; int sum;   main(){  sum = 0;  for(i=1;i <= 10 ;i++){  sum += i;  print("$sum");  }  print(\'sum = $sum\'); } \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '\n 程式碼: \n void main() {  print("Hello, World!"); } \n \n 程式碼: \n int i; int sum;   main(){  sum = 0;  for(i=1;i <= 10 ;i++){  sum += i;  print("$sum");  }  print(\'sum = $sum\'); } \n \n \n \n \n', 'tags': '', 'url': 'About.html'}]};
